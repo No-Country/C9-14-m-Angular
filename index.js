510c3550fe30446dff2d73b97f3d5601e403af29
Seba3585 <sebafraga0@gmail.com>
2023-02-10 21:38:09 +0100

    test app

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express= require('express')
+const http = require('http')
 const bodyParser = require('body-parser')
 const userRouter = require('./src/routes/user.js')
 const filmRouter = require('./src/routes/film.js')
 const reviewRouter = require('./src/routes/review.js')
 
 const app = express()
-app.listen(4000,()=>console.log("running on port 4000"))
+
+const server = http.createServer(app)
+
+server.listen(4000,()=>console.log("running on port 4000"))
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use("/user",userRouter)
 app.use("/film",filmRouter)
 app.use("/review",reviewRouter)
+app.get("/test",(req,res)=>{
+    console.log("testing route")
+    return res.json({message : "test route"})
+} )
