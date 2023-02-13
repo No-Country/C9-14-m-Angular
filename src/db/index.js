c320cecefb76a3b785eb5cfc281326cd3df89cbd
Seba3585 <sebafraga0@gmail.com>
2023-02-13 11:23:12 +0100

    added provisory db connection

--- src/db/index.js
+++ src/db/index.js
@@ -3,17 +3,38 @@ const { DB_USER, DB_HOST, DB_PASS, DB_NAME, DB_PORT, APP_ENV } = require('../con
 const Sequelize = require('sequelize');
 
 
-const pool = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-});
+let pool
+
+if (APP_ENV === 'development') {
+
+
+   pool = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  });
+
+} else {
+
+   pool = new Sequelize(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:5432/${DB_NAME}?sslmode=no-verify`, {
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  });
+
+}
+
+
 
 
 module.exports = {pool}
