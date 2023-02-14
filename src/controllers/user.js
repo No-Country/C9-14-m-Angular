73ded617dd3aa7b2402ac8b81019f994831f3a41
Seba3585 <sebafraga0@gmail.com>
2023-02-14 01:38:01 +0100

    addlog

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -307,7 +307,7 @@ const googleSignIn = async (req,res) => {
     const accessTokenCookieOptions= {
         maxAge: 900000, // 15 mins
         httpOnly: false,
-        domain: "localhost",
+        domain: "localhost:3000",
         path: "/",
         sameSite: "none",
         secure: false,
