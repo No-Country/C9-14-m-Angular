d5ebaa42cc9d070a3210cdbfd0a06ef022df3d3d
Seba3585 <sebafraga0@gmail.com>
2023-02-23 02:25:32 +0100

    sprint3

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 const userRouter = require('./src/routes/user.js')
 const filmRouter = require('./src/routes/film.js')
 const reviewRouter = require('./src/routes/review.js')
+const listRouter = require('./src/routes/list.js')
+
 
 const app = express()
 app.listen(4000,()=>console.log("running on port 4000"))
@@ -13,3 +15,4 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use("/user",userRouter)
 app.use("/film",filmRouter)
 app.use("/review",reviewRouter)
+app.use("/list", listRouter)
