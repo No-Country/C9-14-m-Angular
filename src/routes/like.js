95c706d03741e7146f990803b9c988f7faf2d870
Seba3585 <sebafraga0@gmail.com>
2023-02-26 23:24:12 +0100

    like route

---
+++ src/routes/like.js
@@ -0,0 +1,12 @@
+const express = require ('express')
+const { getUserLikes, pushLike } = require('../controllers/like')
+
+const router = express.Router()
+
+
+router.get('/client/:id', getUserLikes)
+
+router.post('/', pushLike)
+
+
+module.exports = router
